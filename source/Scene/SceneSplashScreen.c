1173dd55d897c3f3749d3d7d683d94553e4a0641
MauroDeRosa <it.mauro.derosa@gmail.com>
2022-05-05 11:20:08 +0200

    fixed vscode snippet, refactored main.c, Empty Scene

--- source/Scene/SceneSplashScreen.c
+++ source/Scene/SceneSplashScreen.c
@@ -4,7 +4,7 @@
 
 #include <raylib.h>
 #include <raymath.h>
-#include "raygui.h"
+#include "raylib-extra/raygui.h"
 
 #include <stdlib.h>
 #include <math.h>
@@ -16,52 +16,46 @@ typedef struct
     int example;
 } SceneSplashScreenData;
 
+// dynamic allocation and modules initial configuration for the current scene here
 void SceneSplashScreenOnInit (void** _$)
 {
     *_$ = malloc( sizeof(SceneSplashScreenData) );
-
-    // dynamic allocation and modules initial configuration for the current scene here 
     
 }
 
+// variables setup and initial state configuration here
 void SceneSplashScreenOnStart (void** _$)
 {
     SceneSplashScreenData *$ = *_$;
-
-    // variables setup and initial state configuration here 
     
 }
 
+// gets executed every frame, update scene state
 void SceneSplashScreenOnUpdate (void** _$)
 {
     SceneSplashScreenData *$ = *_$;
-
-    // gets executed every frame, update scene state 
     
 }
 
+// gets executed every SCENE_MANAGER_FIXEDUPDATE_STEP, update scene state
 void SceneSplashScreenOnFixedUpdate (void** _$)
 {
     SceneSplashScreenData *$ = *_$;
-
-    // gets executed every SCENE_MANAGER_FIXEDUPDATE_STEP, update scene state 
     
 }
 
+// gets executed every frame, draw your privimitives, text and sprites
 void SceneSplashScreenOnRender (void** _$)
 {
     SceneSplashScreenData *$ = *_$;
     ClearBackground(RAYWHITE);
-
-    // gets executed every frame, draw your privimitives, text and sprites 
     
 }
 
+// free dynamically allocated memory and reset modules configuration to default here
 void SceneSplashScreenOnExit (void** _$)
 {
     SceneSplashScreenData *$ = *_$;
-
-    // free dynamically allocated memory and reset modules configuration to default here 
     
     free($);
 }
