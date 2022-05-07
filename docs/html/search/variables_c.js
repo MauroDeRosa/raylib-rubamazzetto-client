7533bd2c464572ac55570603c9799e21eb215972
MauroDeRosa <it.mauro.derosa@gmail.com>
2022-05-08 01:19:27 +0200

    i hope this is the last documentation commit lol

--- docs/html/search/variables_c.js
+++ docs/html/search/variables_c.js
@@ -5,5 +5,5 @@ var searchData=
   ['render_2',['Render',['../structScene__t.html#a4516ec6494a2248f05fdcf09edda54c4',1,'Scene_t']]],
   ['resourcemanager_3',['ResourceManager',['../ResourceManager_8h.html#a812f87e969e6ed96741ab63a86adfaa2',1,'ResourceManager():&#160;ResourceManager.c'],['../ResourceManager_8c.html#a812f87e969e6ed96741ab63a86adfaa2',1,'ResourceManager():&#160;ResourceManager.c']]],
   ['resourcemanagermap_4',['ResourceManagerMap',['../ResourceManager_8h.html#ac6b8a7b4856975b29faab063eafd116f',1,'ResourceManagerMap():&#160;ResourceManager.c'],['../ResourceManager_8c.html#ac6b8a7b4856975b29faab063eafd116f',1,'ResourceManagerMap():&#160;ResourceManager.c']]],
-  ['resourcetypestring_5',['ResourceTypeString',['../ResourceManager_8h.html#a78a6320fae4963480cbd4303dcfd08a2',1,'ResourceTypeString():&#160;ResourceManager.h'],['../ResourceManager_8c.html#a78a6320fae4963480cbd4303dcfd08a2',1,'ResourceTypeString():&#160;ResourceManager.c']]]
+  ['resourcetypestring_5',['ResourceTypeString',['../ResourceManager_8h.html#a78a6320fae4963480cbd4303dcfd08a2',1,'ResourceTypeString():&#160;ResourceManager.c'],['../ResourceManager_8c.html#a78a6320fae4963480cbd4303dcfd08a2',1,'ResourceTypeString():&#160;ResourceManager.c']]]
 ];
