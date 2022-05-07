90391d7006a387ccec84e86a96b838a2b5b6e9cb
MauroDeRosa <it.mauro.derosa@gmail.com>
2022-05-07 19:07:18 +0200

    fixed vscode scene snippets

---
+++ source/Scene/NewScene.c
@@ -0,0 +1,32 @@
+#include "Scene/NewScene.h" 
+
+SCENE_DATA_BEGIN 
+
+// insert your scene variables here
+int example;
+SCENE_DATA_END 
+
+void NewSceneStart()
+{
+    
+}
+
+void NewSceneUpdate()
+{
+    
+}
+
+void NewSceneFixedUpdate()
+{
+    
+}
+
+void NewSceneRender()
+{
+    
+}
+
+void NewSceneExit()
+{
+    
+}
