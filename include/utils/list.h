ff18518b1ca08f996676013b4ec1e5e61294af83
davee <it.davide.leone@gmail.com>
2022-05-17 21:19:13 +0200

    First declaration of header list 	nuovo file:             include/utils/list.h

---
+++ include/utils/list.h
@@ -0,0 +1,76 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include <stdlib.h>
+
+struct listNode_t
+{
+    void *item;
+    struct listNode *next;
+};
+
+/**
+ * @brief
+ *
+ * @param head
+ */
+void listPushFront(struct listNode_t **head, void *);
+
+/**
+ * @brief
+ *
+ * @param head
+ */
+void listPushBack(struct listNode_t *head, void *);
+
+/**
+ * @brief
+ *
+ * @param head
+ * @return void* memory allocation is let to be freed by the user
+ */
+void *listPopFront(struct listNode_t **head);
+
+/**
+ * @brief
+ *
+ * @param head
+ * @return void* memory allocation is let to be freed by the user
+ */
+void *listPopBack(struct listNode_t *head);
+
+/**
+ * @brief
+ *
+ * @param head
+ * @return void* memory allocation is let to be freed by the user
+ */
+void *listDeleteFront(struct listNode_t **head);
+
+/**
+ * @brief
+ *
+ * @param head
+ * @return void* memory allocation is let to be freed by the user
+ */
+void *listDeleteBack(struct listNode_t *head);
+
+/**
+ * @brief
+ *
+ * @param head
+ * @param index
+ * @return void* memory allocation is let to be freed by the user
+ */
+void *listDeleteByIndex(struct listNode_t **head, size_t index);
+
+/**
+ * @brief
+ *
+ * @param head
+ * @param index
+ * @return item at the specified index
+ */
+void *listGetByIndex(struct listNode_t **head, size_t index);
+
+#endif /* __LIST_H__ */
